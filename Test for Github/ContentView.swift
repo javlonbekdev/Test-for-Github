6c062f1c3eebebde97a1dc3b7189ef219d2e1223
javlonbekdev <javlonbekdev@gmail.com>
2025-01-15 13:00:06 +0500

    Initial Commit

---
+++ Test for Github/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test for Github
+//
+//  Created by Javlonbek Dev on 15/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test for Github/Test_for_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_for_GithubApp.swift
+//  Test for Github
+//
+//  Created by Javlonbek Dev on 15/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Test_for_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
