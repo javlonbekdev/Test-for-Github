738c55c0afc71273edc4dcb50dbd5c58861a6258
javlonbekdev <javlonbekdev@gmail.com>
2025-01-15 13:33:19 +0500

    added master view

---
+++ Test for Github/MasterView.swift
@@ -0,0 +1,18 @@
+//
+//  MasterView.swift
+//  Test for Github
+//
+//  Created by Javlonbek Dev on 15/01/25.
+//
+
+import SwiftUI
+
+struct MasterView: View {
+    var body: some View {
+        Text("Hello, Master View!")
+    }
+}
+
+#Preview {
+    MasterView()
+}
